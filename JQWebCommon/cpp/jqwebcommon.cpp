23ccf32a0ebed1485c685d2811397eba824a0f58
Jason <188080501@qq.com>
2024-09-19 16:14:49 +0800

    修复字体引用路径问题

--- JQWebCommon/cpp/jqwebcommon.cpp
+++ JQWebCommon/cpp/jqwebcommon.cpp
@@ -8,8 +8,8 @@ namespace JQWebCommon
 {
 
 void initFont(QGuiApplication *app)
-{    
-    QFontDatabase::addApplicationFont( ":/qml/NotoMono.ttf" );
+{
+    QFontDatabase::addApplicationFont( ":/jqwebcommonfonts/NotoMono.ttf" );
     QFontDatabase::addApplicationFont( ":/jqwebcommonfonts/MiSans-Regular.ttf" );
 
     QFont font( "MiSans", 12 );
